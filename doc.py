e439c7ad821ce7e43ef16ddbbcc13018eae7c39f
Marek Sarvas <sarvas.sarvas@gmail.com>
2021-01-15 22:27:50 +0100

    report done

--- doc.py
+++ doc.py
@@ -50,7 +50,7 @@ def severity_wrt_cause(df, accidents, region='CZ', save_fig=None, show_fig=False
     """
     df = select_region(df, region)
     
-    sev_df = pd.DataFrame({ 'cause': df['p12'], 'dead': df['p13a'], 'severely_injured': df['p13b'], 'lightly_injured': df['p13c']})
+    sev_df = pd.DataFrame({ 'cause': df['p12'], 'mrtvy': df['p13a'], 'tazko_zraneny': df['p13b'], 'lahko_zraneny': df['p13c']})
     cause_labels = {'1':'nezavineny_vodicom', '2': 'neprimerana_rychlost', '3': 'nespravne_predbiehanie', '4': 'nedanie_prednosti_v_jazde', 
                     '5': 'nespravny_sposob_jazdy', '6': 'technicka_zavada'}
     
@@ -79,14 +79,14 @@ def severity_wrt_cause(df, accidents, region='CZ', save_fig=None, show_fig=False
     # print info
     p_injured = cause_df[cause_df['cause'] != 'nezavineny_vodicom'].people.sum()
     print('Počet zranených ľudí pri nehode spôsobenej vodičom {}(pri {:2.4}% nehod)'.format(p_injured, (p_injured/accidents)*100))
-    print('Počet usmrtených ľudí pri nehode spôsobenej vodičom {}'.format(cause_df[(cause_df['cause'] != 'nezavineny_vodicom') & (cause_df['severity'] == 'dead')].people.sum()))
+    print('Počet usmrtených ľudí pri nehode spôsobenej vodičom {}'.format(cause_df[(cause_df['cause'] != 'nezavineny_vodicom') & (cause_df['severity'] == 'mrtvy')].people.sum()))
     print_table(cause_df) 
     
     # setup plot
     fig, ax = plt.subplots(1, 1)
     fig.set_figwidth(10)
     fig.set_figheight(6)
-    fig.suptitle(f'Nasledky nehod v {region} sposobenych roznymi pricinami nehody')
+    fig.suptitle(f'Následky nehôd v {region} spôsobených rôznymi príčinami')
 
     # create pallete
     c_pallet = sns.color_palette("mako", n_colors=3)
@@ -100,9 +100,9 @@ def severity_wrt_cause(df, accidents, region='CZ', save_fig=None, show_fig=False
         ax.annotate(int(p.get_height()-1), xy=(p.xy[0]+p.get_width()/2, p.get_y()+p.get_height()), fontsize=6, ha='center')
     
     # create legend
-    labels = ['usmrteny', 'tazko zraneny', 'lahko zraneny']
+    labels = ['usmrtení', 'tažko zranení', 'ľahko zranení']
     h, l = ax.get_legend_handles_labels()
-    legend = ax.legend(h, labels, title="Zavaznost zraneni",loc='upper right', fontsize=10, frameon=False)
+    legend = ax.legend(h, labels, title="Závažnosť zranení",loc='upper right', fontsize=10, frameon=False)
     plt.setp(legend.get_title(),fontsize=10)
    
 
@@ -110,7 +110,7 @@ def severity_wrt_cause(df, accidents, region='CZ', save_fig=None, show_fig=False
     ax.spines['right'].set_visible(False)
     ax.spines['top'].set_visible(False)
 
-    ax.set_ylabel('pocet zranenych ludi', fontsize=8)
+    ax.set_ylabel('Počet zranených ľudí', fontsize=10)
     ax.set_xlabel('', fontsize=0)
     ax.tick_params(axis="x", labelsize=10)
     ax.tick_params(axis="y", labelsize=10)
@@ -134,8 +134,8 @@ def accidents_cause(df, region='CZ', save_fig=None, show_fig=False):
     caused_by_df = acc_df.groupby('caused_by').cause.agg('count').to_frame('accidents').reset_index()
 
     # change "cause_by" column lables to text description
-    caused_by_labels = {'1':'vodicom motor. vozdila', '2': 'vodicom nemotor. vozdila', '3': 'chodcom', '4': 'zverou', '5': 'inym ucastnikom provozu', 
-                        '6': 'zavadou komunikacie', '7': 'zavadou vozidla', '0': 'ine'}
+    caused_by_labels = {'1':'vodičom motor. vozdila', '2': 'vodičom nemotor. vozdila', '3': 'chodcom', '4': 'zverou', '5': 'iným účastníkom provozu', 
+                        '6': 'závadou komunikácie', '7': 'závadou vozidla', '0': 'iné'}
     
     # sort by number of accidents
     caused_by_df = caused_by_df.sort_values(['accidents'], ascending=False).reset_index(drop=True)
@@ -144,9 +144,9 @@ def accidents_cause(df, region='CZ', save_fig=None, show_fig=False):
 
     # print values used in report
     print(f'Celkovy pocet nehod v {region}: {caused_by_df.accidents.sum()}')
-    print('Pocet nehod zavinenych soferom motor. vozdila: {:2.4}%({})'.format(caused_by_df[caused_by_df.caused_by == 1].percent.tolist()[0], int(caused_by_df[caused_by_df.caused_by == 1].accidents.tolist()[0])))
-    print('Pocet nehod zavinenych soferom nemotor. vozdila: {:2.4}%({})'.format(caused_by_df[caused_by_df.caused_by == 2].percent.tolist()[0], int(caused_by_df[caused_by_df.caused_by == 2].accidents.tolist()[0])))
-    print('Pocet nehod zavinenych zverou: {:2.4}%({})'.format(caused_by_df[caused_by_df.caused_by == 3].percent.tolist()[0], int(caused_by_df[caused_by_df.caused_by == 3].accidents.tolist()[0])))
+    print('Počet nehôd zavinených šoférom motor. vozdila: {:2.4}%({})'.format(caused_by_df[caused_by_df.caused_by == 1].percent.tolist()[0], int(caused_by_df[caused_by_df.caused_by == 1].accidents.tolist()[0])))
+    print('Počet nehôd zavinených šoférom nemotor. vozdila: {:2.4}%({})'.format(caused_by_df[caused_by_df.caused_by == 2].percent.tolist()[0], int(caused_by_df[caused_by_df.caused_by == 2].accidents.tolist()[0])))
+    print('Počet nehôd zavinených zverou: {:2.4}%({})'.format(caused_by_df[caused_by_df.caused_by == 3].percent.tolist()[0], int(caused_by_df[caused_by_df.caused_by == 3].accidents.tolist()[0])))
     
     # change values to labels
     caused_by_df['caused_by'] = caused_by_df['caused_by'].apply(lambda x: caused_by_labels[str(x)])
@@ -156,7 +156,7 @@ def accidents_cause(df, region='CZ', save_fig=None, show_fig=False):
         # plot graph
         fig, ax = plt.subplots(1, 1, figsize=(12, 7))
         fig.subplots_adjust(left=0.17)  # adjust space between figure title and first subplot
-        fig.suptitle('Následky nehôd v jednotlivých regiónoch')
+        fig.suptitle('Počet nehôd vzhľadom na vinníka')
         
         # seaborn style
         
@@ -168,7 +168,7 @@ def accidents_cause(df, region='CZ', save_fig=None, show_fig=False):
             ax.annotate(int(p.get_width()), xy=(p.get_width()+p.get_width()/5, p.xy[1]+p.get_height()/2), fontsize=7, ha='center')
 
         #ax.title.set_text(titles[i])
-        ax.set(ylabel='Zavinenie', xlabel='Pocet nehod')
+        ax.set(ylabel='Zavinenie', xlabel='Počet nehôd')
         ax.spines['right'].set_visible(False)
         ax.spines['top'].set_visible(False)
         plt.setp(ax.get_yticklabels(), rotation=30, fontsize=7)
@@ -184,8 +184,5 @@ def accidents_cause(df, region='CZ', save_fig=None, show_fig=False):
 if __name__ == '__main__':
     df = pd.read_pickle("accidents.pkl.gz")
     df = clean_data(df)
-    accidents = accidents_cause(df, region='JHM', show_fig=True)
+    accidents = accidents_cause(df, region='JHM')
     severity_wrt_cause(df, accidents, save_fig='figures/fig.png', show_fig=True, region='JHM')
-    
-    #accidents_wrt_weather(df)
-    #accidents_wrt_weather(df, save_fig='figures/pocasie.png', show_fig=True)
